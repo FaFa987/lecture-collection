6a93b6404ff7692c40a061b1334a88c1dba87d3e
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-12 00:19:18 +0100

    iterator.next()

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,7 @@ package se.lexicon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -29,9 +30,36 @@ public class Main {
         numbers.add(100);
         numbers.add(300);
         numbers.add(200);
-        numbers.add(700);
+        numbers.add(100);
         numbers.add(500);
         System.out.println(numbers);
+        System.out.println("numbers.size() = " + numbers.size());
+
+        System.out.println("Accese Element with index 1 : " + numbers.get(0));
+        System.out.println("Accese Element with index 3 : " + numbers.get(3));
+        //System.out.println("Accese Element with index 5 : " + numbers.get(5));
+
+        Iterator<Integer> iterator = numbers.iterator();
+        System.out.println("----- modify or delete -----");
+        while (iterator.hasNext()){
+            Integer number = iterator.next();
+            if (number == 300){
+                iterator.remove();
+            } else {
+                System.out.println(numbers);
+            }
+        }
+
+
+        System.out.println("-----for print out, not modify -----");
+        for (Integer number : numbers) {
+            if (number == 200){
+                System.out.println(number);
+            }
+            System.out.println(number);
+        }
+
+
     }
 
 }
