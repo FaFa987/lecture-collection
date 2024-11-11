8be31fdbcc5f2ba3c9b4495d011a2b62dd061326
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-11 22:24:57 +0100

    addChar Arrays

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,11 +1,25 @@
 package se.lexicon;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ex0();
 
-        System.out.printf("Hello and welcome!");
+    }
 
+    public static void ex0(){
+        char[] letters = {'J','A','V','A'};
+        System.out.println(Arrays.toString(letters));
+        letters = addChar('!',letters);
+        System.out.println(Arrays.toString(letters));
+    }
+    private static char[] addChar(char letter, char[] array){
+        int newLength = array.length;
+        var newArray =Arrays.copyOf(array, newLength+1); //var (char[])
+        newArray[newLength] = letter;
+        return newArray;
     }
 }
